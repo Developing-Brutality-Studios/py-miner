d4c89f93edf085e7eca7ae6c65e546308a094c32
pelanguero <pelanguero@gmail.com>
2023-07-09 21:13:20 -0500

    commit inicial

---
+++ main.py
@@ -0,0 +1,46 @@
+import pygame
+
+
+WIDTH = 1920
+HEIGHT = 1080
+
+SPRITE_WIDTH = 128
+SPRITE_HEIGHT = 128
+halfscale=2
+# SPRITE_SCALED_W=SPRITE_WIDTH/
+# SPRITE_SCALED_H=
+# Inicializar Pygame
+pygame.init()
+
+# Crear la ventana
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Renderizar Spritesheet")
+
+# Cargar la spritesheet
+spritesheet = pygame.image.load("./Spritesheets/spritesheet_tiles.png").convert()
+scaledSpriteSheet = pygame.transform.scale(spritesheet,((spritesheet.get_width()/halfscale),(spritesheet.get_height()/halfscale)))
+
+# Definir la posición y dimensiones de la sección de la spritesheet a renderizar
+sprite_rect = pygame.Rect(0, 0, SPRITE_WIDTH/halfscale, SPRITE_HEIGHT/halfscale)
+sprite_rectt = pygame.Rect(0, 0, SPRITE_WIDTH, SPRITE_HEIGHT)
+# Variable para controlar el bucle principal
+running = True
+
+# Bucle principal
+while running:
+    # Manejo de eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Limpiar la ventana
+    window.fill((0, 0, 0))
+
+    # Renderizar la sección de la spritesheet en la ventana
+    window.blit(scaledSpriteSheet, (0, 0), sprite_rect)
+    window.blit(spritesheet, (64, 0), sprite_rectt)
+    # Actualizar la ventana
+    pygame.display.flip()
+
+# Finalizar Pygame
+pygame.quit()
